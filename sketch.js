1d1e8007590c74a5b3fab646144306338cadeca8
Reka <rekapap28@gmail.com>
2019-01-06 21:28:28 +0000

    adds loading of img.jpg

--- sketch.js
+++ sketch.js
@@ -1,7 +1,11 @@
-function setup() {
+var img;
 
+function setup() {
+  createCanvas(512, 683);
+  img = loadImage("assets/img.jpg");
 }
 
 function draw() {
-
+  image(img, 0, 0);
+  image(img, 0);
 }
